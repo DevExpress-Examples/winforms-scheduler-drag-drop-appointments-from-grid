3155940c37fe6177f68872738aceb61c38d0ff38
DevExpressExampleBot <fakeEmail>
2023-08-30 18:08:58 +0400

    Source auto update [skip ci]

--- VB/T179722/Properties/AssemblyInfo.vb
+++ VB/T179722/Properties/AssemblyInfo.vb
@@ -1,23 +1,23 @@
-﻿Imports System.Reflection
-Imports System.Runtime.CompilerServices
+Imports System.Reflection
 Imports System.Runtime.InteropServices
+
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("T179722")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("T179722")>
-<Assembly: AssemblyCopyright("Copyright ©  2014")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
+<Assembly:AssemblyTitle("T179722")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("")>
+<Assembly:AssemblyProduct("T179722")>
+<Assembly:AssemblyCopyright("Copyright ©  2014")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
-<Assembly: ComVisible(False)>
+<Assembly:ComVisible(False)>
 ' The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("e67da2a7-2ce4-4283-9dc6-5a5d7ffed1de")>
+<Assembly:Guid("e67da2a7-2ce4-4283-9dc6-5a5d7ffed1de")>
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
@@ -25,5 +25,5 @@ Imports System.Runtime.InteropServices
 '      Build Number
 '      Revision
 '
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
